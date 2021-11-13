fb62f135817ebb082a7a895da92115b1cfc6ddb7
sarperkaya01 <sarperkaya01@gmail.com>
2021-11-13 22:04:49 +0300

    firstCommit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int c = 0;
+            bool b = true;
+            ArrayList ar = new ArrayList();
+
+            while (b)
+            {
+                if (n / 2 == 1)
+                {
+                    c = n / 2;
+                    ar.Add(n % 2);
+                    ar.Add(c);
+                    break;
+
+                }
+                else
+                {
+                    c = n % 2;
+
+                    n = n / 2;
+
+                }
+
+                ar.Add(c);
+            }
+
+            ar.Reverse();
+
+            foreach (var a in ar)
+            {
+                Console.Write(a);
+            }
+
+            
+            Console.ReadLine();
+        }
+        
+    }
+
+}
